755181d742d981ecfff6f18b3a0dbe503eeeb158
10alexandrov <10alexandrov@gmail.com>
2025-03-04 18:55:03 +0100

    creando el command y el job para corregir los fallos de los tickets y sus auxiliares

---
+++ app/Jobs/FixBugsJob.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Artisan;
+
+class FixBugsJob implements ShouldQueue
+{
+    use Queueable;
+
+    protected $data;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Artisan::call('miniprometeo:fix-bugs-command');
+    }
+}
